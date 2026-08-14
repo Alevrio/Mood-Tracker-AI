8c5b022f4b0fb560b91a6bbb978105464eff27e9
Alevrio <princepanlilio4@gmail.com>
2026-08-14 19:27:57 +0800

    Add mood frequency and most common mood analytics

--- analysis.py
+++ analysis.py
@@ -46,4 +46,15 @@ def analyzeMood():
         percent = (mood_count[mood] /total) * 100
         print(f"{mood}: {percent:.2f}")
 
-        
+def count_moods(df):
+    return df['mood'].value_counts()
+
+def most_common_mood(df):
+    mood_counts = count_moods(df)
+    if mood_counts.empty:
+            return None
+    
+    highest_count = mood_counts.max()
+    highest_mood = mood_counts[mood_counts == highest_count]
+                                
+    return highest_mood.index.tolist(), int(highest_count)
